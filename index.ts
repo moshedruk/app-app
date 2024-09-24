d35e8b9954a74cd455a33b1131956ebb058e2dfc
moshedruk <dmd6708713@gmail.com>
2024-09-24 17:36:41 +0300

    finish controller

--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
 import exp, {Express} from 'express'
 import 'dotenv/config'
 
+import postController from './src/controllers/postController.js'
+import userController from './src/controllers/userController.js'
+import aothController from './src/controllers/authController.js'
+
 const app:Express = exp()
 
+app.use('post',postController)
+app.use('user',userController)
+app.use('auth',aothController)
 
 
 
@@ -12,4 +19,4 @@ const app:Express = exp()
 
 
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT, ():void =>console.log(`server is listen... ${process.env.PORT}`))
